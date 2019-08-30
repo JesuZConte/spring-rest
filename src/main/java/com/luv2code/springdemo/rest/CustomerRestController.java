b3c75ebffd93197bcc135b7cc7e2a2733cab585b
Jesus Zuniga <jesus.zuniga@macys.com>
2019-08-30 12:15:59 -0400

    Adding new endpoints for one and all customers

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,16 @@ public class CustomerRestController {
     @Autowired
     private CustomerService customerService;
 
-    // add mapping for GET / customers
+    // add mapping for GET /customers
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    // add mapping for GET /customers/{customerId}
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
+        Customer theCustomer = customerService.getCustomer(customerId);
+        return theCustomer;
     }
 }
